f1b3bde3c1e3b9ec33e53e537c2a1703d09b5607
Your Name <you@example.com>
2020-02-24 16:21:06 -0500

    cleared code for web28 start

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import {Route} from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path="/">
-      <Home/>
-    </Route>
-    <Route exact path = "/about">
-      <About/>
-    </Route>
-    <Route exact path = "/contact">
-      <Contact/>
-    </Route>
   </div>
 );
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
+
 import React from 'react';
-import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
@@ -7,13 +7,13 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-        <Link to = "/">Home</Link>
+          <a href="">Home</a>
         </div>
         <div>
-        <Link to = "/about">About</Link>
+          <a href="">About</a>
         </div>
         <div>
-        <Link to = "/contact">Contact</Link>
+          <a href="">Contact</a>
         </div>
       </div>
     </div>
